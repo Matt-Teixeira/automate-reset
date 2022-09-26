aa43f5776d11a1d465a28478294e1ab0bd1be247
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2022-09-26 18:19:20 +0000

    Offset time used to determine run error based on time zone

--- utils/timeMatch.js
+++ utils/timeMatch.js
@@ -5,12 +5,47 @@ const timeMatch = () => {
   let timeMatch;
 
   if (minute >= 15 && minute < 45) {
-    return (timeMatch = `${hour}:15`);
+    timeMatch = `${hour}:15`;
   } else if (minute > 15 && minute >= 45) {
-    return (timeMatch = `${hour}:45`);
+    timeMatch = `${hour}:45`;
   } else {
-    return (timeMatch = `${hour - 1}:45`);
+    timeMatch = `${hour - 1}:45`;
   }
+
+  if (timeMatch[0] >= 1 && timeMatch[1] === ":") {
+    timeMatch = "0" + timeMatch;
+  }
+
+  return timeMatch;
 };
 
-module.exports = timeMatch
+let time = timeMatch();
+console.log(time);
+
+const timeZoneOffset = {
+  CDT: 1,
+  MDT: 2,
+  PDT: 3,
+};
+
+const offsetTime = (timeZone) => {
+  let newTime = "";
+  let time = timeMatch();
+
+  if (timeZone === "EDT") {
+    return time;
+  }
+
+  offset = time.split(":")[0];
+  offset = parseInt(time) - timeZoneOffset[timeZone];
+
+  newTime = offset.toString() + ":" + time.split(":")[1];
+
+  if (newTime[0] >= 1 && newTime[1] === ":") {
+    newTime = "0" + newTime;
+  }
+
+  return newTime;
+};
+
+module.exports = { timeMatch, timeZoneOffset, offsetTime };
