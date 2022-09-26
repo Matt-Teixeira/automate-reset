340555bcbc6a09b5ae57d35985c7cb0efa8a09bb
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2022-09-26 18:20:45 +0000

    Time comparison no longer handled within uptimeObjects

--- utils/uptimeObjects.js
+++ utils/uptimeObjects.js
@@ -1,19 +1,21 @@
 
-const uptimeObjects = (SMEs, time, [reSME, re4, re5]) => {
+const uptimeObjects = (SMEs, [reSME, re4, re5, re6]) => {
     let uptimes = [];
 
     for (let sme of SMEs) {
       let smeNum = sme.match(reSME);
       let logfiles = sme.match(re4);
+      let timeZone = sme.match(re6);
 
       let data = {
+        time_zone: timeZone[0].split(" ")[1],
         sme: smeNum[0],
         log_files: logfiles,
-        runOnTime: [],
+        run_time: [],
       };
       for (let log of logfiles) {
         const lastRunTime = log.match(re5);
-        data.runOnTime.push(lastRunTime[0] === time);
+        data.run_time.push(lastRunTime[0]);
       }
       uptimes.push(data);
     }
