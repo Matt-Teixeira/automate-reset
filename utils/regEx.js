6a59c3789f0b437ccf5ab33ad6b131179a64ec5f
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2022-09-26 18:16:20 +0000

    Added matches for secondary networkBlock and time_zone

--- utils/regEx.js
+++ utils/regEx.js
@@ -2,9 +2,11 @@ const all_sme_regEx = /SME\d{5}/;
 const sme_blocks_regEx = /ok:\s\[SME\d{5}\]\s=>.*?\}/g;
 const logFiles_regEx = /"-rw-.*?\.log/g;
 const logFile_time_regEx = /\d{2}:\d{2}/g;
-const networkBlock = /en\w+?:.*?collisions/g;
-const matchInet = /inet\s\d/
+const networkBlock = /en\w+?\s+.*?collisions/g; // was /en\w+?:.*?collisions/g; not every ifconfig output adapter ends with ":"
+const networkBlock2 = /en\w+?:.*?collisions/g;
+const matchInet = /inet\s/; //was /inet\s\d/
 const networkAdapter = /en\w+/;
+const time_zone_regEx = /\s[A-Z]{3}/;
 
 module.exports = {
   all_sme_regEx,
@@ -12,6 +14,8 @@ module.exports = {
   logFiles_regEx,
   logFile_time_regEx,
   networkBlock,
+  networkBlock2,
   networkAdapter,
-  matchInet
+  matchInet,
+  time_zone_regEx,
 };
